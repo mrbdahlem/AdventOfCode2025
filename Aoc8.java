1f534ccf4c87574f8563bb6afadbf54a8f40cfa3
Brian Dahlem <dahlem.brian@cusd80.com>
2025-12-08 20:45:23 +0000

    commenting on day 8, prep for day 9

--- Aoc8.java
+++ Aoc8.java
@@ -52,12 +52,11 @@ public class Aoc8 {
     }
 
     /**
+     * Parse the input data into useful structures
      * @param data raw input data 
-     * 
      */
     public static void prepare(String data) {
-
-        long start = System.nanoTime();
+        // long start = System.nanoTime();
 
         String[] lines = data.split("\\R");
         points = new Point3D[lines.length];
@@ -65,7 +64,7 @@ public class Aoc8 {
             points[i] = Point3D.from(lines[i]);
         }
 
-        long parse = System.nanoTime();
+        // long parse = System.nanoTime();
 
         connectedPairs = new ArrayList<>(points.length * (points.length - 1) / 2);
         for (int j = 0; j < points.length; j++) {
@@ -75,12 +74,12 @@ public class Aoc8 {
             }
         }
         
-        long pairs = System.nanoTime();
+        // long pairs = System.nanoTime();
 
         // Sort pairs by distance smallest to largest
         connectedPairs.sort((a, b) -> Long.compare(a.distance(), b.distance()));
 
-        long sort = System.nanoTime();
+        // long sort = System.nanoTime();
 
         circuits = new ArrayList<>(points.length);
         circuitMap = new HashMap<>(points.length);
@@ -91,17 +90,17 @@ public class Aoc8 {
             circuitMap.put(p, c);
         }
 
-        long circuitsTime = System.nanoTime();
+        // long circuitsTime = System.nanoTime();
 
-        System.out.println("Parsing duration: " + (parse - start) / 1000 / 1000 + "ms");
-        System.out.println("Pairing duration: " + (pairs - parse) / 1000 / 1000 + "ms");
-        System.out.println("Sorting duration: " + (sort - pairs) / 1000 / 1000 + "ms");
-        System.out.println("Circuit initialization duration: " + (circuitsTime - sort) / 1000 / 1000 + "ms");
+        // System.out.println("Parsing duration: " + (parse - start) / 1000 / 1000 + "ms");
+        // System.out.println("Pairing duration: " + (pairs - parse) / 1000 / 1000 + "ms");
+        // System.out.println("Sorting duration: " + (sort - pairs) / 1000 / 1000 + "ms");
+        // System.out.println("Circuit initialization duration: " + (circuitsTime - sort) / 1000 / 1000 + "ms");
     }
 
     /**
-     * @param points junction box locations 
-     * 
+     * Connect the closest pairs of points into circuits
+     * @param numPairs number of pairs to connect
      */
     public static void partOne(int numPairs) {
         // Connect the closest pairs of points into circuits
@@ -133,8 +132,7 @@ public class Aoc8 {
     }
 
     /**
-     * @param points 
-     * 
+     * Connect the closest pairs of points into circuits until all points are connected in one circuit
      */
     public static void partTwo() {
         // Connect the closest pairs of points into circuits
@@ -155,10 +153,6 @@ public class Aoc8 {
             i++;
         }
         
-        if (pair == null) {
-            throw new IllegalStateException("No pairs were connected");
-        }
-        
         // Calculate the distance from the wall to the last pair
         long total = pair.p1().x() * pair.p2().x();
 
@@ -166,11 +160,19 @@ public class Aoc8 {
     }
 }
 
+/**
+ * A pair of 3D points and their squared distance
+ */
 record Pair(Point3D p1, Point3D p2, long distance) {
     public Pair(Point3D p1, Point3D p2) {
         this(p1, p2, p1.distanceSquared(p2));
     }
 
+    /**
+     * Check if this pair is equal to another pair (regardless of order)
+     * @param o the other pair
+     * @return true if the pairs are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -181,12 +183,25 @@ record Pair(Point3D p1, Point3D p2, long distance) {
     }
 }
 
+/**
+ * A point in 3D space
+ */
 record Point3D(int x, int y, int z) {
+    /**
+     * Create a Point3D from a comma-separated string
+     * @param s the string
+     * @return the Point3D
+     */
     public static Point3D from (String s) {
         String[] parts = s.split(",");
         return new Point3D(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
     }
 
+    /**
+     * Calculate the squared distance to another point
+     * @param other the other point
+     * @return the squared distance
+     */
     public long distanceSquared(Point3D other) {
         long dx = this.x - other.x;
         long dy = this.y - other.y;
@@ -195,21 +210,35 @@ record Point3D(int x, int y, int z) {
     }
 }
 
+/**
+ * A circuit of connected points
+ */
 class Circuit {
-    Set<Point3D> points = new LinkedHashSet<>();
+    // The points in the circuit
+    private Set<Point3D> points = new LinkedHashSet<>();
     
+    /**
+     * Add a point to the circuit
+     * @param p the point to add
+     */
     public void add(Point3D p) {
         points.add(p);
     }
 
+    /**
+     * Get the size of the circuit
+     * @return the number of points in the circuit
+     */
     public int size() {
         return points.size();
     }
 
-    public boolean contains(Point3D p) {
-        return points.contains(p);
-    }
-
+    /**
+     * Merge this circuit with another circuit
+     * @param other the other circuit
+     * @param circuitMap the map of points to circuits
+     * @return the merged circuit
+     */
     public Circuit merge(Circuit other, Map<Point3D,Circuit> circuitMap) {
         Circuit larger = this.size() >= other.size() ? this : other;
         Circuit smaller = this.size() >= other.size() ? other : this;
