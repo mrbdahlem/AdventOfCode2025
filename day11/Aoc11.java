72a476ed1fb3ea657f9de4bdd362e55c232c5da7
Brian Dahlem <dahlem.brian@cusd80.com>
2025-12-10 14:07:01 -0700

    prep day 11

---
+++ day11/Aoc11.java
@@ -0,0 +1,61 @@
+package day11;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Advent of Code 2025 - Day 11
+ * @author Brian Dahlem
+ * 
+ * Brief description of the puzzle.
+ */
+public class Aoc11 {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Load the data
+        File inputFile = new File("sample11.txt");
+        Scanner scan = new Scanner(inputFile);
+       
+        // Load the entire data as one token
+        scan.useDelimiter("\\A");
+        String data = scan.next();
+        scan.close();
+
+        // Solve the two parts of the puzzle
+        long start = System.nanoTime();
+        prepare(data);
+        long prepped = System.nanoTime();
+        partOne();
+        long first = System.nanoTime();
+        partTwo();
+        long sec = System.nanoTime();
+
+        System.out.println("Preparation duration: " + (prepped - start) / 1000 / 1000 + "ms");
+        System.out.println("Part 1 duration: " + (first - prepped) / 1000 / 1000 + "ms");
+        System.out.println("Part 2 duration: " + (sec - first) / 1000 / 1000 + "ms");
+    }
+
+    /**
+     * @param data raw input data
+     */
+    public static void prepare(String data) {
+
+    }
+
+    /**
+     * 
+     */
+    public static void partOne() {
+        long total = 0;
+
+        System.out.println("Part 1: {" + total + "}");
+    }
+
+    /**
+     * 
+     */
+    public static void partTwo() {
+        long total = 0;
+
+        System.out.println("Part 2: {" + total + "}");
+    }
+}
